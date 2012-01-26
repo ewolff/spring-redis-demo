42da3a8cd48cffbafb4cd083a0abd52f21c20414
Eberhard Wolff <eberhard.wolff@gmail.com>
2012-01-26 15:49:17 +0100

    Fixed bug - afterPropertiesSet() must be called

--- src/main/java/de/adesso/redis/RedisEMailReaderMessaging.java
+++ src/main/java/de/adesso/redis/RedisEMailReaderMessaging.java
@@ -7,21 +7,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 public class RedisEMailReaderMessaging {
 
-	
 	public void handleMessage(String msg) {
 		System.out.println(msg);
 	}
-	
+
 	public static void main(String[] args) {
-		 RedisMessageListenerContainer listenerContainer = RedisHelper
+		RedisMessageListenerContainer listenerContainer = RedisHelper
 				.setupRedisListenerContainer();
-		 MessageListenerAdapter listener = new MessageListenerAdapter();
-		 listener.setDelegate(new RedisEMailReaderMessaging());
-		 listener.setDefaultListenerMethod("handleMessage");
-		 listener.setSerializer(new StringRedisSerializer());
-		listenerContainer.addMessageListener(listener,	 new ChannelTopic("emails"));
-		 listenerContainer.afterPropertiesSet();
-		 listenerContainer.start();
+		MessageListenerAdapter listener = new MessageListenerAdapter();
+		listener.setDelegate(new RedisEMailReaderMessaging());
+		listener.setDefaultListenerMethod("handleMessage");
+		listener.setSerializer(new StringRedisSerializer());
+		listener.afterPropertiesSet();
+		listenerContainer.addMessageListener(listener, new ChannelTopic(
+				"emails"));
+		listenerContainer.afterPropertiesSet();
+		listenerContainer.start();
 	}
 
 }
